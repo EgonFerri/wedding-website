8e601f62d1dfc66ae83f6597d3bd26013ac509ee
EgonFerri <egon.ferri@gmail.com>
2025-03-25 15:55:11 +0100

    fix: google map

--- js/scripts.js
+++ js/scripts.js
@@ -187,17 +187,6 @@ $(document).ready(function () {
     $('.player').YTPlayer();
 
 
-    /********************** Toggle Map Content **********************/
-// $('#btn-show-map').click(function () {
-//     $('#map-content').toggleClass('toggle-map-content');
-//     $('#btn-show-content').toggleClass('toggle-map-content');
-// });
-// $('#btn-show-content').click(function () {
-//     $('#map-content').toggleClass('toggle-map-content');
-//     $('#btn-show-content').toggleClass('toggle-map-content');
-// });
-
-
     /********************** Add to Calendar **********************/
     var myCalendar = createCalendar({
         options: {
