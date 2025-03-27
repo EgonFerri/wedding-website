d362ef41072effffbf3f7787142e2983d7333755
EgonFerri <egon.ferri@gmail.com>
2025-03-27 11:02:39 +0100

    rsvp form working

--- js/scripts.js
+++ js/scripts.js
@@ -226,26 +226,27 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '8512aaeb58c10e0e5f8254bb61bbe268'
+            && MD5($('#invite_code').val()) !== '8512aaeb58c10e0e5f8254bb61bbe268') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    if (data.result === "error") {
-                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+            $.post('https://script.google.com/macros/s/AKfycbwolaLmeUORhPHwVQ5w0wqk4Kdhk7st0M11aaUGZeWJGPpfdvVghGWvdIOJPHFKETW1/exec', data)
+                .done(function (response) {
+                    console.log(response);
+                    if (response.result === "error") {
+                        $('#alert-wrapper').html(alert_markup('danger', response.message));
                     } else {
                         $('#alert-wrapper').html('');
                         $('#rsvp-modal').modal('show');
                     }
                 })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                .fail(function (error) {
+                    console.log(error);
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server.'));
                 });
         }
     });
+
 
 });
 
